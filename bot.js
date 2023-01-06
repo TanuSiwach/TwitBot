0f243c19debc5f9dcc7dd7aa03a1c9275f304fd7
TanuSiwach <tanusiwach2000@gmail.com>
2023-01-06 23:50:04 +0530

    added env file

--- bot.js
+++ bot.js
@@ -1,7 +1,12 @@
 console.log("Hey, I'm TwitBot");
 var Twit = require('twit');
-var config = require('./config');
-var T = new Twit(config);
+require('dotenv').config();
+var T = new Twit({
+    consumer_key: process.env.consumer_key,
+    consumer_secret: process.env.consumer_secret,
+    access_token: process.env.access_token,
+    access_token_secret: process.env.access_token_secret
+})
 var retweet = function() {
     var params = {
         q: '#webdevelopment',
